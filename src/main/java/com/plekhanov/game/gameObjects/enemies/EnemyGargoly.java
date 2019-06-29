e35fd7da3bebd40c6f20908da3601187a041161a
Plekhanov <atagan@rambler.ru>
2019-06-29 14:28:11 +0300

    Добавлены фазы гаргулье

--- src/main/java/com/plekhanov/game/gameObjects/enemies/EnemyGargoly.java
+++ src/main/java/com/plekhanov/game/gameObjects/enemies/EnemyGargoly.java
@@ -1,5 +1,6 @@
 package com.plekhanov.game.gameObjects.enemies;
 
+import com.plekhanov.game.Game;
 import com.plekhanov.game.ImageLoader;
 import com.plekhanov.game.Model;
 
@@ -9,24 +10,34 @@ public class EnemyGargoly extends Enemy {
     private static final int imageWidth = 500;
     private static final int imageHeight = 500;
     private static final int renderOrder = 20;
-    private static final double FLY_SPEED = 0.9;
-    private static final double WALL_SPEED = 0.7;
+    private static final double FLY_SPEED = 1;
+    private static final double WALK_SPEED = 0.6;
 
 
     private boolean lookRight = true;
     private boolean fly = true;
     private boolean walk = false;
 
+
+    //счетчики
     private int flyCount = 0;
     private int MAX_FLY_COUNT = 320;
 
     private int walkCount = 0;
     private int MAX_WALK_COUNT = 160;
 
+    private boolean flyPhase = true;
+    private int flyPhaseCount = 0;
+    private int MAX_FLY_PHASE_COUNT = (int) Game.UPDATES * 3;
+
+    private boolean walkPhase = false;
+    private int walkPhaseCount = 0;
+    private int MAX_WALK_PHASE_COUNT = (int) Game.UPDATES * 3;
+
 
     public EnemyGargoly(double x, double y, double speedX, double speedY, Model model) {
         super(x, y, speedX, speedY, ImageLoader.getGargolyFlyRight_1(), imageWidth, imageHeight, renderOrder, model);
-        life = 3;
+        life = 10;
     }
 
 
@@ -44,14 +55,15 @@ public class EnemyGargoly extends Enemy {
 
 
     private void changeAction() {
-        if (x > 1900) {
-            speedX = -FLY_SPEED;
-            lookRight = false;
+
+        if (walkPhase) {
+            walkPhase();
         }
-        if (x < -200) {
-            speedX = FLY_SPEED;
-            lookRight = true;
+
+        if (flyPhase) {
+            flyPhase();
         }
+
     }
 
     private void changeImage() {
@@ -133,10 +145,65 @@ public class EnemyGargoly extends Enemy {
         if (walkCount > MAX_WALK_COUNT) {
             walkCount = 0;
         }
+
+        //счетчик fly фазы
+        if (fly) {
+            flyPhaseCount++;
+            if (flyPhaseCount > MAX_FLY_PHASE_COUNT) {
+                flyPhaseCount = 0;
+
+                walkPhase = true;
+                flyPhase = false;
+            }
+        }
+
+        //счетчик walk фазы
+        if (walk) {
+            walkPhaseCount++;
+            if (walkPhaseCount > MAX_WALK_PHASE_COUNT) {
+                walkPhaseCount = 0;
+
+                walkPhase = false;
+                flyPhase = true;
+            }
+        }
+    }
+
+
+    private void walkPhase() {
+        fly = false;
+        walk = true;
+
+        setSpeed(WALK_SPEED);
+    }
+
+    private void flyPhase() {
+        fly = true;
+        walk = false;
+
+        setSpeed(FLY_SPEED);
+    }
+
+
+    private void setSpeed(double speed) {
+        if (lookRight) {
+            speedX = speed;
+        } else {
+            speedX = -speed;
+        }
+
+        if (x > 1800) {
+            speedX = -speed;
+            lookRight = false;
+        }
+        if (x < 100) {
+            speedX = speed;
+            lookRight = true;
+        }
     }
 
     private void checkClashWithPlayer() {
-        if (Math.abs(model.getPlayer().getX() - getX()) < 80 && Math.abs(model.getPlayer().getY() - getY()) < 80) {
+        if (Math.abs(model.getPlayer().getX() - getX()) < 100 && Math.abs(model.getPlayer().getY() - getY()) < 100) {
             model.getPlayer().minusLife();
         }
     }
