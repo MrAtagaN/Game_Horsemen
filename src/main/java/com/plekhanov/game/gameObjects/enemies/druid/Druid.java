dd66dbeeb306d53275fa43c158e171191c68c113
Plekhanov <atagan@rambler.ru>
2019-12-12 01:50:38 +0300

    Enemy Druid

--- src/main/java/com/plekhanov/game/gameObjects/enemies/druid/Druid.java
+++ src/main/java/com/plekhanov/game/gameObjects/enemies/druid/Druid.java
@@ -14,15 +14,18 @@ public class Druid extends Enemy {
 
     private static final int SPEED_OF_CHANGE_WALK_IMAGES = 80;
     private static final int SPEED_OF_CHANGE_SHOOT_IMAGES = 120;
+    private static final int SPEED_OF_CHANGE_SUMMON_IMAGES = 100;
+
     private static final int MAX_WALK_COUNT = SPEED_OF_CHANGE_WALK_IMAGES * 4;
     private static final int MAX_SHOOT_COUNT = SPEED_OF_CHANGE_SHOOT_IMAGES * 3;
+    private static final int MAX_SUMMON_COUNT = SPEED_OF_CHANGE_SUMMON_IMAGES * 5;
 
 
     private boolean lookRight = false;
     private int walkCount;
     private int shootCount;
     private int summonCount;
-    private Phase phase = Phase.SHOOT;
+    private Phase phase = Phase.SUMMON;
 
     private enum Phase {
         WALK,
@@ -68,6 +71,19 @@ public class Druid extends Enemy {
                     bufferedImage = ImageLoader.getDruid_shoot_left_3();
                 }
             }
+            if (phase == Phase.SUMMON) {
+                if (summonCount < SPEED_OF_CHANGE_SUMMON_IMAGES) {
+                    bufferedImage = ImageLoader.getDruid_summon_left_1();
+                } else if (summonCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 2) {
+                    bufferedImage = ImageLoader.getDruid_summon_left_2();
+                } else if (summonCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 3) {
+                    bufferedImage = ImageLoader.getDruid_summon_left_3();
+                } else if (summonCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 4) {
+                    bufferedImage = ImageLoader.getDruid_summon_left_4();
+                } else if (summonCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 5) {
+                    bufferedImage = ImageLoader.getDruid_summon_left_5();
+                }
+            }
 
         } else {
             if (phase == Phase.WALK) {
@@ -90,6 +106,19 @@ public class Druid extends Enemy {
                     bufferedImage = ImageLoader.getDruid_shoot_right_3();
                 }
             }
+            if (phase == Phase.SUMMON) {
+                if (summonCount < SPEED_OF_CHANGE_SUMMON_IMAGES) {
+                    bufferedImage = ImageLoader.getDruid_summon_right_1();
+                } else if (summonCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 2) {
+                    bufferedImage = ImageLoader.getDruid_summon_right_2();
+                } else if (summonCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 3) {
+                    bufferedImage = ImageLoader.getDruid_summon_right_3();
+                } else if (summonCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 4) {
+                    bufferedImage = ImageLoader.getDruid_summon_right_4();
+                } else if (summonCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 5) {
+                    bufferedImage = ImageLoader.getDruid_summon_right_5();
+                }
+            }
         }
 
 
@@ -105,5 +134,10 @@ public class Druid extends Enemy {
         if (shootCount > MAX_SHOOT_COUNT) {
             shootCount = 0;
         }
+        summonCount++;
+        if (summonCount > MAX_SUMMON_COUNT) {
+            summonCount = 0;
+        }
+
     }
 }
