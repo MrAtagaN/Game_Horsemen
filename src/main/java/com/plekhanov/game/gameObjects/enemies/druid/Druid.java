80ba7dd92dc6df1402d74b89be12d4090ad6e9fc
Plekhanov <atagan@rambler.ru>
2019-12-12 07:04:58 +0300

    Enemy Druid

--- src/main/java/com/plekhanov/game/gameObjects/enemies/druid/Druid.java
+++ src/main/java/com/plekhanov/game/gameObjects/enemies/druid/Druid.java
@@ -15,10 +15,12 @@ public class Druid extends Enemy {
     private static final int RENDER_ORDER = 10;
     private static final int LIFE = 3;
 
+    /** Частота смены картинок */
     private static final int SPEED_OF_CHANGE_WALK_IMAGES = 80;
     private static final int SPEED_OF_CHANGE_SHOOT_IMAGES = 120;
     private static final int SPEED_OF_CHANGE_SUMMON_IMAGES = 100;
 
+    /** Максимальное значение счетчика смены картинки */
     private static final int MAX_WALK_IMAGE_COUNT = SPEED_OF_CHANGE_WALK_IMAGES * 4;
     private static final int MAX_SHOOT_IMAGE_COUNT = SPEED_OF_CHANGE_SHOOT_IMAGES * 3;
     private static final int MAX_SUMMON_IMAGE_COUNT = SPEED_OF_CHANGE_SUMMON_IMAGES * 5;
@@ -34,23 +36,20 @@ public class Druid extends Enemy {
      * Последовательность фаз
      */
     private List<Phase> phases = Arrays.asList(Phase.WALK, Phase.SHOOT, Phase.WALK, Phase.SUMMON);
-    private int phaseCount = 0;
+    private int phaseNumber = 0;
 
     private boolean lookRight = false;
 
     /**
-     * Счетчики смены картинок
+     * Счетчик смены картинки
      */
-    private int walkImageCount;
-    private int shootImageCount;
-    private int summonImageCount;
+    private int imageCount;
 
     /**
-     * Счетчики смены фаз
+     * Счетчик смены фаз
      */
-    private int walkPhaseCount;
-    private int shootPhaseCount;
-    private int summonPhaseCount;
+    private int phaseCount;
+
     private Phase phase = Phase.WALK;
 
     private enum Phase {
@@ -71,7 +70,6 @@ public class Druid extends Enemy {
         checkClashWithPlayer(160, 160);
         checkClashWithPlayerShoot(40, 100);
         action();
-        changePhase();
         changeImage();
         incrementCount();
     }
@@ -80,101 +78,75 @@ public class Druid extends Enemy {
     private void action() {}
 
 
-    private void changePhase() {
-        if (walkPhaseCount > MAX_WALK_PHASE_COUNT) {
-            walkImageCount = 0;
-            walkPhaseCount = 0;
-            phase = nextPhase();
-        }
-        if (shootPhaseCount > MAX_SHOOT_PHASE_COUNT) {
-            shootImageCount = 0;
-            shootPhaseCount = 0;
-            phase = nextPhase();
-        }
-        if (summonPhaseCount > MAX_SUMMON_PHASE_COUNT) {
-            summonImageCount = 0;
-            summonPhaseCount = 0;
-            phase = nextPhase();
-        }
-    }
-
-
-    private Phase nextPhase() {
-        phaseCount++;
-        if (phaseCount >= phases.size()) {
-            phaseCount=0;
-        }
-        return phases.get(phaseCount);
-    }
 
 
     private void changeImage() {
         if (!lookRight) {
             if (phase == Phase.WALK) {
-                if (walkImageCount < SPEED_OF_CHANGE_WALK_IMAGES) {
+                if (imageCount < SPEED_OF_CHANGE_WALK_IMAGES) {
                     bufferedImage = ImageLoader.getDruid_walk_left_1();
-                } else if (walkImageCount < SPEED_OF_CHANGE_WALK_IMAGES * 2) {
+                } else if (imageCount < SPEED_OF_CHANGE_WALK_IMAGES * 2) {
                     bufferedImage = ImageLoader.getDruid_walk_left_2();
-                } else if (walkImageCount < SPEED_OF_CHANGE_WALK_IMAGES * 3) {
+                } else if (imageCount < SPEED_OF_CHANGE_WALK_IMAGES * 3) {
                     bufferedImage = ImageLoader.getDruid_walk_left_3();
-                } else if (walkImageCount < SPEED_OF_CHANGE_WALK_IMAGES * 4) {
+                } else if (imageCount < SPEED_OF_CHANGE_WALK_IMAGES * 4) {
                     bufferedImage = ImageLoader.getDruid_walk_left_2();
                 }
             }
             if (phase == Phase.SHOOT) {
-                if (shootImageCount < SPEED_OF_CHANGE_WALK_IMAGES) {
+                if (imageCount < SPEED_OF_CHANGE_SHOOT_IMAGES) {
                     bufferedImage = ImageLoader.getDruid_shoot_left_2();
-                } else if (shootImageCount < SPEED_OF_CHANGE_WALK_IMAGES * 2) {
+                } else if (imageCount < SPEED_OF_CHANGE_SHOOT_IMAGES * 2) {
                     bufferedImage = ImageLoader.getDruid_shoot_left_1();
-                } else if (shootImageCount < SPEED_OF_CHANGE_WALK_IMAGES * 3) {
+                } else if (imageCount < SPEED_OF_CHANGE_SHOOT_IMAGES * 3) {
                     bufferedImage = ImageLoader.getDruid_shoot_left_3();
                 }
             }
             if (phase == Phase.SUMMON) {
-                if (summonImageCount < SPEED_OF_CHANGE_SUMMON_IMAGES) {
+                if (imageCount < SPEED_OF_CHANGE_SUMMON_IMAGES) {
                     bufferedImage = ImageLoader.getDruid_summon_left_1();
-                } else if (summonImageCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 2) {
+                } else if (imageCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 2) {
                     bufferedImage = ImageLoader.getDruid_summon_left_2();
-                } else if (summonImageCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 3) {
+                } else if (imageCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 3) {
                     bufferedImage = ImageLoader.getDruid_summon_left_3();
-                } else if (summonImageCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 4) {
+                } else if (imageCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 4) {
                     bufferedImage = ImageLoader.getDruid_summon_left_4();
-                } else if (summonImageCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 5) {
+                } else if (imageCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 5) {
                     bufferedImage = ImageLoader.getDruid_summon_left_5();
                 }
             }
 
         } else {
             if (phase == Phase.WALK) {
-                if (walkImageCount < SPEED_OF_CHANGE_WALK_IMAGES) {
+                if (imageCount < SPEED_OF_CHANGE_WALK_IMAGES) {
                     bufferedImage = ImageLoader.getDruid_walk_right_1();
-                } else if (walkImageCount < SPEED_OF_CHANGE_WALK_IMAGES * 2) {
+                } else if (imageCount < SPEED_OF_CHANGE_WALK_IMAGES * 2) {
                     bufferedImage = ImageLoader.getDruid_walk_right_2();
-                } else if (walkImageCount < SPEED_OF_CHANGE_WALK_IMAGES * 3) {
+                } else if (imageCount < SPEED_OF_CHANGE_WALK_IMAGES * 3) {
                     bufferedImage = ImageLoader.getDruid_walk_right_3();
-                } else if (walkImageCount < SPEED_OF_CHANGE_WALK_IMAGES * 4) {
+                } else if (imageCount < SPEED_OF_CHANGE_WALK_IMAGES * 4) {
                     bufferedImage = ImageLoader.getDruid_walk_right_2();
                 }
             }
             if (phase == Phase.SHOOT) {
-                if (shootImageCount < SPEED_OF_CHANGE_WALK_IMAGES) {
+                if (imageCount < SPEED_OF_CHANGE_SHOOT_IMAGES) {
                     bufferedImage = ImageLoader.getDruid_shoot_right_2();
-                } else if (shootImageCount < SPEED_OF_CHANGE_WALK_IMAGES * 2) {
+                } else if (imageCount < SPEED_OF_CHANGE_SHOOT_IMAGES * 2) {
                     bufferedImage = ImageLoader.getDruid_shoot_right_1();
-                } else if (shootImageCount < SPEED_OF_CHANGE_WALK_IMAGES * 3) {
+                } else if (imageCount < SPEED_OF_CHANGE_SHOOT_IMAGES * 3) {
                     bufferedImage = ImageLoader.getDruid_shoot_right_3();
                 }
             }
             if (phase == Phase.SUMMON) {
-                if (summonImageCount < SPEED_OF_CHANGE_SUMMON_IMAGES) {
+                if (imageCount < SPEED_OF_CHANGE_SUMMON_IMAGES) {
                     bufferedImage = ImageLoader.getDruid_summon_right_1();
-                } else if (summonImageCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 2) {
+                } else if (imageCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 2) {
                     bufferedImage = ImageLoader.getDruid_summon_right_2();
-                } else if (summonImageCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 3) {
+                } else if (imageCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 3) {
                     bufferedImage = ImageLoader.getDruid_summon_right_3();
-                } else if (summonImageCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 4) {
+                } else if (imageCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 4) {
                     bufferedImage = ImageLoader.getDruid_summon_right_4();
-                } else if (summonImageCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 5) {
+                } else if (imageCount < SPEED_OF_CHANGE_SUMMON_IMAGES * 5) {
                     bufferedImage = ImageLoader.getDruid_summon_right_5();
                 }
             }
@@ -185,28 +157,44 @@ public class Druid extends Enemy {
 
     @Override
     protected void incrementCount() {
-        //Инкремент счетчиков фаз и картинок
+        imageCount++;
+        phaseCount++;
+        //Смена фаз
         if (phase == Phase.WALK) {
-            walkPhaseCount++;
-            walkImageCount++;
-            if (walkImageCount > MAX_WALK_IMAGE_COUNT) {
-                walkImageCount = 0;
+            if (imageCount > MAX_WALK_IMAGE_COUNT) {
+                imageCount = 0;
+            }
+            if (phaseCount > MAX_WALK_PHASE_COUNT) {
+                changePhase();
             }
         }
         if (phase == Phase.SHOOT) {
-            shootPhaseCount++;
-            shootImageCount++;
-            if (shootImageCount > MAX_SHOOT_IMAGE_COUNT) {
-                shootImageCount = 0;
+            if (imageCount > MAX_SHOOT_IMAGE_COUNT) {
+                imageCount = 0;
+            }
+            if (phaseCount > MAX_SHOOT_PHASE_COUNT) {
+                changePhase();
             }
         }
         if (phase == Phase.SUMMON) {
-            summonPhaseCount++;
-            summonImageCount++;
-            if (summonImageCount > MAX_SUMMON_IMAGE_COUNT) {
-                summonImageCount = 0;
+            if (imageCount > MAX_SUMMON_IMAGE_COUNT) {
+                imageCount = 0;
+            }
+            if (phaseCount > MAX_SUMMON_PHASE_COUNT) {
+                changePhase();
             }
         }
 
     }
+
+    private void changePhase() {
+        imageCount = 0;
+        phaseCount = 0;
+        phaseNumber++;
+        if (phaseNumber >= phases.size()) {
+            phaseNumber=0;
+        }
+        phase = phases.get(phaseNumber);
+    }
+
 }
